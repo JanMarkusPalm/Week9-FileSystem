a246242389bed6c52914f0e63ef40ea0648858ac
Opilane TTHK <opilane@tthk.ee>
2021-10-27 11:31:28 +0300

    lists

---
+++ Lists/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myWishes = new List<string>();
+            myWishes.Add("fresh new cool mints");
+            myWishes.Add("mercury");
+            myWishes.Add("gaming chair");
+
+            int listLenght = myWishes.Count;
+            Console.WriteLine($"List lenght : {listLenght}");
+            //Console.WriteLine(myWishes[0]);
+            //Console.WriteLine(myWishes[1]);
+            //Console.WriteLine(myWishes[2]);
+
+            foreach (string wish in myWishes)
+            {
+                Console.WriteLine($"you wish for : {wish}");
+
+            }
+        }
+    }
+}
---
+++ ReadingFromFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples\wishlist";
+            string userfile = "Wishlist";
+            string fullfilepath = $@"{rootDirectory}\{userfile}.txt";
+
+            string[] dataFromFile = File.ReadAllLines(fullfilepath);
+
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine();
+            dataFromFile[0] = "pink bruh";
+
+            foreach (string line in dataFromFile)
+            {
+                Console.WriteLine(line);
+            }
+
+            File.WriteAllLines(fullfilepath, dataFromFile);
+        }
+    }
+}
